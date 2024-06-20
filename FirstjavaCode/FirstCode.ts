54c287358a7214ddbbde7e4e7305ab2c7ad29218
JustinFrei <justin.frei05@gmail.com>
2024-06-20 13:40:41 +0200

    operators

--- FirstjavaCode/FirstCode.ts
+++ FirstjavaCode/FirstCode.ts
@@ -1,1 +1,20 @@
 console.log("Hello World from TypeScript");
+let a = 3;
+let b = 2;
+console.log("a = 3 and b = 2");
+console.log("Addition: a + b =", a+b);
+console.log("Subtraction: a - b =", a-b);
+console.log("Multiplication: a * b =", a*b);
+console.log("Division: a / b =", a/b);
+console.log("Modulus: a % b =", a%b);
+console.log("Increment: a++ =", a++);
+console.log("Decrement: b-- =", b--);
+console.log("a >(=) b =", a>=b);
+console.log("a <(=) b =", a<=b);
+console.log("a == b =", a == b);
+console.log("a != b =", a!=b);
+console.log("a != b && b  <= a", a != b && b <= a);
+console.log("a > b || b == a", a>b || b==a);
+console.log("!(a == 2) =", !(a==2));
+
+
