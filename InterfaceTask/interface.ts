fe47e9aef86f387a9c0695284e0f3a11f27b732d
JustinFrei <justin.frei05@gmail.com>
2024-07-01 15:24:03 +0200

    time to go home

---
+++ InterfaceTask/interface.ts
@@ -0,0 +1,53 @@
+let canvas:HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
+let crc1: CanvasRenderingContext2D = canvas.getContext("2d")!;
+
+interface treeData {
+    height: number;
+    type: string;
+    leafColor: string;
+    width: number;
+    position: number;
+}
+
+let tree1: treeData = {
+    height: 200,
+    type: "needle",
+    leafColor: "green",
+    width: 50,
+    position: 300,
+}
+
+let tree2: treeData = {
+    height: 300,
+    type: "leaf",
+    leafColor: "orange",
+    width: 40,
+    position: 400,
+}
+
+let trees: treeData[] = [tree1, tree2];
+
+for(let i = 0; i < trees.length; i++){
+    if(trees[i].type == "needle"){
+        crc1.fillStyle = "brown";
+        crc1.fillRect(trees[i].position, 600, -trees[i].width, -(trees[i].height / 10));
+        crc1.beginPath();
+        crc1.fillStyle = trees[i].leafColor;
+        crc1.moveTo(trees[i].position - trees[i].width * 0.5, 600 - trees[i].height);
+        crc1.lineTo(trees[i].position + (trees[i].width * 1.5) - trees[i].width * 0.5, 600 - (trees[i].height/ 10));
+        crc1.lineTo(trees[i].position - (trees[i].width * 1.5) - trees[i].width * 0.5, 600 - (trees[i].height/ 10));
+        crc1.closePath();
+        crc1.fill();
+    }
+    //süße laubbäumchen :)
+    else{
+        crc1.fillStyle = "brown";
+        crc1.fillRect(trees[i].position, 600, -trees[i].width, -(trees[i].height * 0.5));
+        crc1.beginPath();
+        crc1.fillStyle = trees[i].leafColor;
+        crc1.ellipse(trees[i].position - (trees[i].width * 0.5), 600 - (trees[i].height * 0.5), (trees[i].width * 2), trees[i].height * 0.25, 0, Math.PI*180, Math.PI*90, true);
+        crc1.closePath();
+        crc1.fill();
+
+    }
+}
---
+++ complexImage/complexImage.js
@@ -0,0 +1,24 @@
+"use strict";
+let canvas = document.getElementsByTagName("canvas")[0];
+let crcb = canvas.getContext("2d");
+crcb.fillStyle = "#eeaf61";
+crcb.fillRect(0, 0, 1500, 700);
+crcb.fillStyle = "#ee5d6c";
+crcb.beginPath();
+crcb.ellipse(Math.random() * 1500, 250, 200, 200, 0, Math.PI * 180, Math.PI * 90, true);
+crcb.fill();
+crcb.closePath();
+crcb.fillStyle = "#afe1e3";
+crcb.fillRect(0, 250, 1500, 1000);
+crcb.fillStyle = "#fdeab7";
+crcb.fillRect(0, 400, 1500, 1000);
+for (let c = 0; c < 4; c++) {
+    crcb.fillStyle = "white";
+    crcb.beginPath();
+    let rndmx = Math.random();
+    crcb.ellipse(100 + rndmx * 1300, 100 + Math.random() * 50, 30 + Math.random() * 50, 20 + Math.random() * 10, 0, Math.PI * 180, Math.PI * 90);
+    crcb.ellipse(100 + rndmx * 1300 + 20, 100 + Math.random() * 50, 40 + Math.random() * 50, 40 + Math.random() * 10, 0, Math.PI * 180, Math.PI * 90);
+    crcb.fill();
+    crcb.closePath();
+}
+crcb.fillStyle = "brown";
